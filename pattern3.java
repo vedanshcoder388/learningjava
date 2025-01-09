f930cba98507dbe438aafbdc1cd7eea5b61fa4d7
vedanshcoder388 <vedansharya388@gmail.com>
2025-01-09 22:39:54 +0530

    pattern3.java
    To print pattern

---
+++ pattern3.java
@@ -0,0 +1,17 @@
+public class number
+{
+    public void main()
+    {
+        int n,i;
+        for(i=1;i<=5;i++)
+        {
+            for(n=1;n<=i;n++)
+        {
+            System.out.print(n);
+        }
+        System.out.println();
+    }
+}
+}
+
+        
