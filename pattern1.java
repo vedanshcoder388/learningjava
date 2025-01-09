7f12c69eddc7f86d2a7a2d7e71628ec6fb634dd9
vedanshcoder388 <vedansharya388@gmail.com>
2025-01-09 22:31:59 +0530

    Create pattern1.java
    To print pattern

---
+++ pattern1.java
@@ -0,0 +1,17 @@
+public class pattern
+{
+    public void main()
+    {
+        int n,i;
+        for(i=1;i<=5;i++)
+        {
+            for(n=1;n<=5;n++)
+        {
+            System.out.print(n+" ");
+        }
+        System.out.println();
+    }
+}
+}
+
+        
