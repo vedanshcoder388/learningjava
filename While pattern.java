4113744331ddfb748526e9b3b2975458af80b3f9
vedanshcoder388 <vedansharya388@gmail.com>
2025-01-10 18:36:07 +0530

    Create While pattern.java

---
+++ While pattern.java
@@ -0,0 +1,36 @@
+public class whilepattern
+{
+    public void main()
+    {
+        int n=1,i=1;
+        while(n<=5)
+    {
+        System.out.println(n+" ");
+        n++;
+    }
+    while(i<=5)
+    {
+      System.out.print(i+" ");
+      i++;
+    }
+  }
+}
+        
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
